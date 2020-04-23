a9eb1b15d7f5ad87927af9447b4918e9ca881945
bruteForceBilly <bruteForceBilly@protonmail.com>
2020-04-23 10:58:39 +0200

    refactor catalog to css grid

--- src/views/Dummy.vue
+++ src/views/Dummy.vue
@@ -1,11 +1,15 @@
 <template>
-  <div class="mt-16 mx-32">
-    <h1 class="font-bold uppercase mb-6">Dummy Catalog Page</h1>
-    <div class="h-screen flex flex-wrap content-start justify-start ml-4">
+  <div class="h-screen pt-4 px-4 sm:px-6 md:px-12 xl:px-16">
+    <h1 class="font-bold uppercase mb-8">
+      Dummy Catalog Page
+      <span class="font-light text-gray-600 text-xs">( 767 Products )</span>
+    </h1>
+    <div class="w-full h-12 bg-orange-400 mb-6"></div>
+    <div class="grid gap-6">
       <div
         v-for="item in products"
         :key="item.productId"
-        class="w-1/5 h-56 pt-16 px-4 bg-gray-200 my-4 mx-4 text-left"
+        class="pt-16 px-4 text-left bg-orange-300"
       >
         <p>{{ item.productTitle }}</p>
       </div>
@@ -29,4 +33,9 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lscoped>
+.grid {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(240px, 1fr));
+}
+</style>
