8b80eb0bb90bfb27989405a37086c261ff426238
bruteForceBilly <bruteForceBilly@protonmail.com>
2021-02-05 19:45:38 +0100

    css done

--- src/components/Catalog/ThePaginationBar.vue
+++ src/components/Catalog/ThePaginationBar.vue
@@ -1,51 +1,60 @@
 <template>
-  <div class="flex flex-row justify-center align-baseline mt-20 mb-64">
-    <span class="mt-4 mr-3 text-sm text-widest text-gray-900">Page:</span>
-    <div
-      class="flex justify-center relative"
-      @mouseover="hover = true"
-      @mouseleave="hover = false"
-    >
-      <button
-        class="outline-none uppercase z-30 border bg-white border-gray-300 py-3 px-6 rounded-none flex items-center w-20"
-      >
-        <span class="pr-1 flex-1 font-medium">
-          {{ paginationMenu.label }}
-        </span>
-
-        <span>
-          <svg
-            class="fill-current h-4 w-4"
-            :class="hover ? 'transform -rotate-180' : ''"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-          >
-            <path
-              d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
-            />
-          </svg>
-        </span>
-      </button>
-
+  <div class="mt-20 mb-64 flex flex-row">
+    <div class="ml-auto flex flex-row justify-center align-baseline">
+      <span class="mt-4 mr-3 text-sm text-widest text-gray-900">Page:</span>
       <div
-        class="position-after-height inline-block z-20 absolute w-20 border-gray-300 outline-none border bg-white rounded-none"
-        :class="hover ? 'inline-block' : 'hidden'"
+        class="flex justify-center relative"
+        @mouseover="hover = true"
+        @mouseleave="hover = false"
       >
-        <ul class="list-outside">
-          <li
-            class="hover:bg-gray-200 w-full py-3"
-            v-for="option in options"
-            :key="option.id"
-          >
-            <label class="custom-label flex justify-center font-medium">
-              <span @click.self="setByRouteHandler()">{{ option.label }} </span>
-            </label>
-          </li>
-        </ul>
+        <button
+          class="outline-none uppercase z-30 border bg-white border-gray-300 py-3 px-6 rounded-none flex items-center w-20"
+        >
+          <span class="pr-1 flex-1 font-medium">
+            {{ paginationMenu.label }}
+          </span>
+
+          <span>
+            <svg
+              class="fill-current h-4 w-4"
+              :class="hover ? 'transform -rotate-180' : ''"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <path
+                d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
+              />
+            </svg>
+          </span>
+        </button>
+
+        <div
+          class="position-after-height inline-block z-20 absolute w-20 border-gray-300 outline-none border bg-white rounded-none"
+          :class="hover ? 'inline-block' : 'hidden'"
+        >
+          <ul class="list-outside">
+            <li
+              class="hover:bg-gray-200 w-full py-3"
+              v-for="option in options"
+              :key="option.id"
+            >
+              <label class="custom-label flex justify-center font-medium">
+                <span @click.self="setByRouteHandler()"
+                  >{{ option.label }}
+                </span>
+              </label>
+            </li>
+          </ul>
+        </div>
       </div>
+      <span class="mt-4 ml-2 text-sm text-widest text-gray-900"
+        >of {{ pagination.pageCount }}
+      </span>
     </div>
-    <span class="mt-4 ml-2 text-sm text-widest text-gray-900"
-      >of {{ pagination.pageCount }}
+    <span
+      class="mt-4 align ml-auto text-sm tracking-widest text-gray-900 font-black underline"
+    >
+      NEXT
     </span>
   </div>
 </template>
