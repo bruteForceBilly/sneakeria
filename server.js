578bceb5f2b7e06898b5600d7365e069f17cfd85
bruteForceBilly <bruteForceBilly@protonmail.com>
2021-03-23 19:57:35 +0100

    connect history api added

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require("express");
+var history = require("connect-history-api-fallback");
 const serveStatic = require("serve-static");
 const path = require("path");
 const app = express();
+
+app.use(
+  history({
+    verbose: true,
+  })
+);
+
 app.use(serveStatic(path.join(__dirname, "dist")));
 const port = process.env.PORT || 80;
 app.listen(port);
