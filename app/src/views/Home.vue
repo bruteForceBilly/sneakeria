8c93b36b0014e54c95589d456761f67da84b8198
bruteForceBilly <bruteForceBilly@protonmail.com>
2021-04-28 15:43:24 +0200

    updated home

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="h-screen flex justify-center ">
-    <div class="text-lg mt-12 pt-10 md:pt-24 px-4 md:px-60 xl:px-0 xl:w-1/4 leading-relaxed">
-      <h1 class="text-xl font-semibold tracking-wide mb-3">👋 Hello there!</h1>
+    <div class="text-lg mt-12 pt-10 md:pt-24 px-4 md:px-60 xl:px-0 xl:w-1/3 leading-relaxed">
+      <h1 class="text-xl font-semibold tracking-wide mb-3">👋 Hi!</h1>
       <p>
-        Welcome to Sneakeria, a <i>sneaky</i> copy of the adidas <a class="underline text-blue-500 visited:text-purple-600" href="https://www.adidas.nl/blauw" target="_blank" title="Check out Adidas catalog page for comparison">catalog page</a>, reverse
-        engineered and built in vue.js, json-server and tailwind. Dockerized with docker
-        compose and deployed to a digital ocean droplet. Product data is seeded from a homegrown data generation tool run
-        by node.js. Click on {{ getSuggestion }} to get started, or why
-        not just check out all <router-link to="/blue" class="underline text-blue-500 visited:text-purple-600">blue</router-link> products?
+        Welcome to Sneakeria, an ecommerce example app that I carefully crafted after the Adidas <a class="underline text-blue-500 visited:text-purple-600" href="https://www.adidas.nl/blauw" target="_blank" title="Check out Adidas catalog page for comparison">catalog page</a>.
+        The purpose of this project was to create an environment that facilitates exploration of more advanced patterns in Vue.js. Product data is seeded from a homegrown data generation tool run by node.js and served by json-server. Tailwind is used for the styling UI. Deployment is done via docker compose to a digital ocean droplet. The project's github repository can be found
+        <a class="underline text-blue-500 visited:text-purple-600" href="https://github.com/bruteForceBilly/sneakeria" target="_blank" title="Github">here</a>.
       </p>
       <br>
-      <p>If you like what you see and are looking for a frontender to hire, feel free to <a class="underline text-blue-500" href="mailto:bruteForceBilly@protonmail.com">reach out!</a></p>
+      <p>If you're looking for a front-end web developer that specializes in vue, with javascript experience from several scale-ups, please <a class="underline text-blue-500" href="mailto:bruteForceBilly@protonmail.com">reach out</a>.</p>
       <br>
-      <p class="leading-loose mb-4">Kind Regards,<br>bruteForceBilly</p>
+      <p>Click this <router-link to="/blue" class="underline text-blue-500 visited:text-purple-600">link</router-link> to start exploring the app</p>
+      <br>
+      <p class="leading-loose mb-4">Best Regards,<br>William</p>
       <p class="text-sm">
         <a href="https://github.com/bruteForceBilly" target="_blank" class="underline text-blue-500 visited:text-purple-600">Github</a><span class="mx-2 text-blue-500 visited:text-purple-600">|</span><a href="https://www.linkedin.com/in/wsaidac/" target="_blank" class="underline text-blue-500">LinkedIn</a></p>
     </div>
